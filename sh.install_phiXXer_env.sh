7f02634b2052cc27821feab25107d1f7508d787a
Amit Sinha <amit.sinhaa@gmail.com>
2024-11-01 12:56:43 -0400

    Update sh.install_phiXXer_env.sh
    include bedtools in phiXXer_env

--- sh.install_phiXXer_env.sh
+++ sh.install_phiXXer_env.sh
@@ -6,6 +6,7 @@ conda create --yes --name phiXXer_env canu minimap2 samtools;
 	conda install --yes -c bioconda perl-bioperl;
 	conda install --yes mummer4 cd-hit
 	conda install --yes seqkit seqtk;
+	conda install --yes bedtools;
 	conda install --yes emboss;
 	conda install --yes -c bioconda gepard;
 # 	conda install --yes last;
